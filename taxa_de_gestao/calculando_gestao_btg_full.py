f9292fdf83911cc26a7a00fa22c405b9bddacbca
lauroteles <laurotfl@gmail.com>
2024-05-08 11:55:27 -0300

    Corrigindo bug

--- taxa_de_gestao/calculando_gestao_btg_full.py
+++ taxa_de_gestao/calculando_gestao_btg_full.py
@@ -45,14 +45,12 @@ class CalculandoTaxadeGestao():
         calculo_diario = 1/252
         dia_e_hora = datetime.datetime.now().strftime("%Y-%m-%d")
 
-        self.planilha_controle['Conta'] = self.planilha_controle['Conta'].astype(str).str[:-2].map(lambda x: '00'+x)
+        self.planilha_controle['Conta'] = self.planilha_controle['Conta']#.astype(str).str[:-2].map(lambda x: '00'+x)
         self.planilha_controle = self.planilha_controle[['Conta','Taxa de Gestão']]
-
         self.planilha_controle.rename(columns={'Taxa de Gestão':'Taxa_de_Gestão','Conta':'conta'},inplace=True)
         tx_gestao = pd.merge(self.planilha_controle,self.pl, left_on='conta',right_on='Conta',how='outer')
         tx_gestao = tx_gestao[['conta','Taxa_de_Gestão','Valor']].rename(columns={'Valor':'VALOR'})
         selecionar_data = st.date_input('Data')
-
         tx_gestao['Data'] = selecionar_data
         tx_gestao['Tx_Gestão_Diaria'] = ((tx_gestao['Taxa_de_Gestão']+1)**calculo_diario-1)*100
         tx_gestao['Valor_de_cobrança'] = round(tx_gestao['VALOR']*(tx_gestao['Tx_Gestão_Diaria'])/100,2)
@@ -116,16 +114,14 @@ class CalculandoTaxadeGestao():
 
         self.planilha_controle = self.planilha_controle[['Cliente','Conta','Taxa de Gestão']].iloc[:-5,:]
 
-        self.pl_agora = self.pl_agora.iloc[3:,:]
+        self.pl_agora = self.pl_agora.iloc[2:,:]
 
 
 
         bovespa_vista_start = self.pl_agora[self.pl_agora == 'BOVESPA A VISTA'].index[0]
         bovespa_vista_end = self.pl_agora[self.pl_agora['Gestora'] == 'BOVESPA OPC'].index[0]
         bovespa_vista_data = self.pl_agora.iloc[bovespa_vista_start :bovespa_vista_end].iloc[:-5]
-    
 
-        #tesouro_direto_start = pl_agora[pl_agora['Unnamed: 2'] == 'Codigo Cliente'].index[0]#.searchsorted(0)[0]
         tesouro_direto_start = self.pl_agora[self.pl_agora['Gestora']=='BMF'].index[-1]
         tesouro_direto_end = self.pl_agora[self.pl_agora['Gestora'] == 'FUNDOS'].index[0]
         tesouro_direto_data = self.pl_agora.iloc[tesouro_direto_start :tesouro_direto_end].iloc[:-5]
@@ -142,7 +138,8 @@ class CalculandoTaxadeGestao():
         garantias_end = self.pl_agora.shape[0]
         garantias_data = self.pl_agora.iloc[garantias_start:garantias_end]
 
-        bovespa_vista_data = bovespa_vista_data.iloc[9:,:]
+        bovespa_vista_data = bovespa_vista_data.iloc[3:,:]
+
         bovespa_vista_data['Valor_pl'] = bovespa_vista_data['Unnamed: 12']*bovespa_vista_data['Unnamed: 13']
 
         bovespa_vista_data_agregado = bovespa_vista_data.groupby('Unnamed: 4')['Valor_pl'].sum().reset_index().rename(columns={'Unnamed: 4':'BLUEMETRIX'})
@@ -153,10 +150,12 @@ class CalculandoTaxadeGestao():
 
         garantias_agregado = garantias_data.groupby('Unnamed: 3')['Unnamed: 12'].sum().reset_index().rename(columns={'Unnamed: 3':'BLUEMETRIX'})
 
+
+        
         tx_gestao= pd.merge(bovespa_vista_data_agregado,tesouro_direto_agregado, on='BLUEMETRIX',how='outer').merge(
             renda_fixa_agregado, on='BLUEMETRIX',how='outer'
         ).merge(garantias_agregado,how='outer',on='BLUEMETRIX')
-       
+
         if 'Nome' in tx_gestao['BLUEMETRIX'].values and 'CPF' in tx_gestao['BLUEMETRIX'].values:
             # Elimine as linhas que contenham 'Cliente' ou 'CPF' nos valores
             tx_gestao= tx_gestao[(tx_gestao['BLUEMETRIX'] != 'Nome') & (tx_gestao['BLUEMETRIX'] != 'CPF')]
@@ -180,18 +179,18 @@ class CalculandoTaxadeGestao():
             'LUIS FERNANDO LEITE SABINO DE OLIVEIRA':"Luis Fernando Leite Sabino de Oliveira",
             'JUAREZ MARIANO MACHADO':'Juarez Mariano Machado'
         }
-
+        
         tx_gestao['BLUEMETRIX'] = tx_gestao['BLUEMETRIX'].replace(arrumar_Clientes)
         colunas_numericas = tx_gestao.select_dtypes(include=[np.number])
         tx_gestao['VALOR'] = round(colunas_numericas.sum(axis=1),2)
+
 
         tx_gestao = tx_gestao.iloc[:,[0,-1]].merge(self.planilha_controle,left_on='BLUEMETRIX',right_on='Cliente',how='left').rename(columns={
             'Taxa de Gestão':'Taxa_de_Gestão',
             'Conta':'conta'
         })
+
         
-
-
         calculo_diario = 1/252
         dia_e_hora = datetime.datetime.now().strftime("%Y-%m-%d")
         selecionar_data = st.date_input('Data')
